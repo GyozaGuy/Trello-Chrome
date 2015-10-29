42146f9523f55bbb8b26ee733d6d0dccd3e737c0
Steven Stromberg <steven.stromberg@intel.com>
2015-10-29 10:44:38 -0600

    Window stays centered

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
+var height = 750;
+var width = 1200;
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create(
-		'index.htm',
-		{id: 'trello', height: 650, width: 1000}
-	);
+	chrome.app.window.create('index.htm', {
+		id: 'trello',
+		outerBounds: {
+	    height: height,
+	    width: width
+	  }
+	},
+	function(win) {
+    win.outerBounds.setPosition(
+      Math.round(screen.availWidth / 2 - width / 2),
+      Math.round(screen.availHeight / 2 - height / 2)
+    );
+	});
 });
